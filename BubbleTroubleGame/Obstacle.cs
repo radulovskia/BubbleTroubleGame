b9c194be787e5b8f7bee2a1eaa0d9d31f4af98c2
Мишева Деспина <despina.misheva@students.finki.ukim.mk>
2022-06-27 21:19:28 +0200

    Obstacle class

---
+++ BubbleTroubleGame/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+public class Obstacle
+{
+    public int width { get; set; }
+	public int height { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Color color = Color.Maroon;
+
+    public Obstacle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public void draw(Graphics graphics)
+    {
+        Brush brush = new SolidBrush(color);
+        graphics.FillRectangle(brush, new Rectangle(X,Y,width,height));
+    }
+}
