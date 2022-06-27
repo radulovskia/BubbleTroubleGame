c6d37d30efa7b01e269cf6d2e8635e34c1eafa6f
somefinkistudent <85944330+somefinkistudent@users.noreply.github.com>
2022-06-27 15:31:15 +0200

    Locked window size and set dimensions to 1280x800

--- BubbleTroubleGame/Form1.Designer.cs
+++ BubbleTroubleGame/Form1.Designer.cs
@@ -32,9 +32,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1262, 753);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
