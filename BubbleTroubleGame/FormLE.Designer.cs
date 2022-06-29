02e452a858dc97e8147298a2ab6ba1b0c239f52e
Kiko <kristijanstameski@gmail.com>
2022-06-29 19:34:21 +0200

    Level Editor

---
+++ BubbleTroubleGame/FormLE.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BubbleTroubleGame
+{
+    partial class FormLE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelUI = new System.Windows.Forms.Panel();
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panelUI.SuspendLayout();
+            this.panelGame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelUI
+            // 
+            this.panelUI.Controls.Add(this.label2);
+            this.panelUI.Controls.Add(this.label1);
+            this.panelUI.Controls.Add(this.textBox2);
+            this.panelUI.Controls.Add(this.textBox1);
+            this.panelUI.Controls.Add(this.button2);
+            this.panelUI.Controls.Add(this.button1);
+            this.panelUI.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelUI.Location = new System.Drawing.Point(0, 480);
+            this.panelUI.Name = "panelUI";
+            this.panelUI.Size = new System.Drawing.Size(640, 81);
+            this.panelUI.TabIndex = 0;
+            // 
+            // panelGame
+            // 
+            this.panelGame.Controls.Add(this.listBox1);
+            this.panelGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGame.Location = new System.Drawing.Point(0, 0);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(640, 480);
+            this.panelGame.TabIndex = 1;
+            this.panelGame.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PLAYTEST";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 63);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "EXIT TO MENU";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(422, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time Limit:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(422, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(359, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score Limit:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(508, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 446);
+            this.listBox1.TabIndex = 3;
+            // 
+            // FormLE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 561);
+            this.Controls.Add(this.panelGame);
+            this.Controls.Add(this.panelUI);
+            this.Name = "FormLE";
+            this.Text = "Form2";
+            this.panelUI.ResumeLayout(false);
+            this.panelUI.PerformLayout();
+            this.panelGame.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelUI;
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ BubbleTroubleGame/FormLE.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleTroubleGame
+{
+    public partial class FormLE : Form
+    {
+        public Scene scene;
+        public FormLE()
+        {
+            InitializeComponent();
+            scene = new Scene();
+            UpdateView();
+        }
+
+        private void panel2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                scene.addBall(new Ball(new Point(e.X, e.Y), 10, 1, 0, 0));
+            } else if(e.Button == MouseButtons.Right)
+            {
+                scene.Select(new Point(e.X, e.Y));
+            }
+            UpdateView();
+        }
+        public void UpdateView()
+        {
+            Bitmap bmp = new Bitmap(panelGame.Width, panelGame.Height);
+            scene.draw(Graphics.FromImage(bmp));
+            panelGame.CreateGraphics().Clear(Color.White);
+            panelGame.CreateGraphics().DrawImageUnscaled(bmp, 0, 0);
+        }
+        // ListBox gi ima site elementi sto mozat da bidat dodadeni, se otvara na tab
+        // Move ke bide so middle mouse
+        // Ke ima izbor za vrednosti vo zavisnost od sto e selektirano
+    }
+}
