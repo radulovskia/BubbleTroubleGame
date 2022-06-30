072f6c25fa9f3b0bd791c527f2bc1a5b7dda1c6b
Despina Misheva <despina.misheva@students.finki.ukim.mk>
2022-06-30 15:33:15 +0200

    Main menu creation and start of app

---
+++ BubbleTroubleGame/MainMenu.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BubbleTroubleGame
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.Start.Location = new System.Drawing.Point(168, 155);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(239, 135);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 441);
+            this.Controls.Add(this.Start);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+    }
+}
---
+++ BubbleTroubleGame/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleTroubleGame
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form2 = new Form1();
+            form2.Closed += (s, args) => this.Close();
+            form2.Show();
+        }
+    }
+}
